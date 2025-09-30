69d6547f67d2249ef1a21c295cebca2bca4b880a
aristring <arianacabreravera@gmail.com>
2025-09-30 18:15:34 -0500

    Fix deploy workflow to use dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ali-Mateo.github.io/', // 👈 ¡esto es esencial para GitHub Pages!
+  base: '/Ali-Mateo.github.io/',
+  build: { outDir: '__temp_dist' }
 })
