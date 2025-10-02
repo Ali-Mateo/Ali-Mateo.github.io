b47a2f4eae2d9a8d563cd2bdf1d75ed62b8e8408
aristring <arianacabreravera@gmail.com>
2025-10-02 13:50:13 -0500

    config vite + gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ali-Mateo.github.io/',
-  build: { outDir: '__temp_dist' }
+  base: '/',            // 👈 al ser un User Page repo
+  build: { outDir: 'dist' } // 👈 usa la carpeta estándar
 })
